5c9e85c88b27acad3960fce837b2bfa56f8021f8
Paula <paula.rezende@usp.br>
2022-08-08 12:38:51 -0300

    Teste botão

--- projetos/retrandom/sketch.js
+++ projetos/retrandom/sketch.js
@@ -4,38 +4,43 @@ let larg;
 let tam = 14;
 let fr = 45;
 let start = true;
-let canvas2;
+let button;
 let vel;
 let r;
 let r2;
 
 function setup() {
-  createCanvas(720, 610);
-  canvas2 = createGraphics(720, 480);
-  canvas2.clear();
-  canvas2.background(255);
+  createCanvas(displayWidth, displayHeight);
   capture = createCapture(VIDEO);
-  capture.size(720, 480);
+  // capture.size(720, 480);
   capture.hide();
-  textFont("Helvetica", 20);
+  
+  button = createButton('Diminuir retângulo');
+  button.position(0, 0);
+  button.size(150,50)
+  button.mousePressed(dimRet);
+  button.style('font-size', '18px');
+
+}
+
+function dimRet() {
+    tam += 1;
+    if (tam > 50) {
+      tam = 50;
+    }
 }
 
 function draw() {
-  frameRate(fr);
-  clear();
-  vel = floor(map(frameRate(), 1, 60, 0, 100));
-  text("Velocidade (de 0 à 100%): " + vel + "%", 10, 520);
-  text("Tamanho do retângulo: " + alt + " x " + larg + " px", 10, 550);
 
   if (start) {
     alt = floor(capture.height / tam);
     larg = floor(capture.width / tam);
     r = int(random(0, capture.height));
     r2 = int(random(0, capture.width));
-    canvas2.copy(capture, int(r2), int(r), larg, alt, int(r2), int(r), larg, alt);
+    copy(capture, int(r2), int(r), larg, alt, int(r2), int(r), larg, alt);
   }
-  image(canvas2, 0, 0);
 }
+
 
 function keyTyped() {
   if (key === "i" || key === "I") {
@@ -51,7 +56,7 @@ function keyTyped() {
       frameRate(fr);
     }
   } else if (key === "x" || key === "X") {
-        canvas2.background(255);
+        background(255);
       }
 }
 
